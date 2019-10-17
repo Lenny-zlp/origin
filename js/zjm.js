0496962b67cc34b64e86add3501fed918b8a1c05
zlp <704792246@qq.com>
2019-10-17 19:12:08 +0800

    移动

---
+++ js/zjm.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+    // -----------------底部----------------
+    // 点击事件
+    var lis = document.querySelectorAll(".lis")
+
+    lis.forEach(function(item, i) {
+        lis[i].onclick = function() {
+
+        }
+
+    })
+
+
+
+
+
+
+
+
+
+
+}
